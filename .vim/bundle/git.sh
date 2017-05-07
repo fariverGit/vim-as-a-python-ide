e64d0eeeb060dfc947719f3ac74b48244bb84437
fariver <wyj1239630590@users.noreply.github.com>
2017-05-07 19:23:49 +0800

    Update git.sh

--- .vim/bundle/git.sh
+++ .vim/bundle/git.sh
@@ -3,3 +3,4 @@ git clone git://github.com/Lokaltog/vim-powerline.git
 git clone https://github.com/kien/ctrlp.vim.git
 git clone https://github.com/klen/python-mode
 git clone git://github.com/davidhalter/jedi-vim.git
+git clone https://github.com/ervandew/supertab
